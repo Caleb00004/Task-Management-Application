22af3f62d4458dd85233eeda8d6fb8a44dbe593c
Akpan Caleb <56205463+Caleb00004@users.noreply.github.com>
2024-08-02 16:37:25 +0100

    Add files via upload

---
+++ frontend/Task Management/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        cu: "1178px", // Define your custom breakpoint value here
+        smx2: "982px",
+        smphone: "340px"
+      },
+      colors: {
+        primary: "#087E18",
+        secondary: "#090F2D",
+        lightBlue: "#38468A",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
+
+export default config;
