943cccbfa2727e4ea8c1f68fa63bd1cde50f29f0
Akpan Caleb <56205463+Caleb00004@users.noreply.github.com>
2024-08-02 17:10:19 +0100

    chore: removed unnecessary comment

--- frontend/Task Management/src/pages/_app.tsx
+++ frontend/Task Management/src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-// import { ContextProvider } from '../../globalContext/ContextProvider'
-// import { DataContextProvider } from '../../globalContext/DataContext'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
 import { taskSlice } from '@/redux/taskSlice'
-// import { apiSlice } from '../../api_feature/apiSLice'
-// import {Toaster} from "react-hot-toast"
 import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,16 +11,4 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </ApiProvider>
   )
-  // return (
-  //   <ApiProvider api={apiSlice}>
-  //     <ContextProvider>
-  //       <DataContextProvider>
-  //         <div className='z-[10]'>
-  //           <Toaster />
-  //         </div>
-  //         <Component {...pageProps} />
-  //       </DataContextProvider>
-  //     </ContextProvider>
-  //   </ApiProvider>
-  // )
 }
